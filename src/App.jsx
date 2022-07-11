be9b8a545a1f3c65a3b23d6cc15bdf8f6f57051e
Gorgan10 <gorgan15@mail.ru>
2022-07-10 20:37:15 -0700

    lesson-1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import './App.css';
+import {Form} from "./components/FormClass/Form";
+
+export class App extends Component {
+  render() {
+    return <>
+      <Form />
+    </>
+  }
+}
